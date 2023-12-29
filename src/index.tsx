f8a4d1c22a33434feb29e5a1a3b435ca68135b71
Marbax <macintosh@MacBook-Air.local>
2023-12-29 15:31:49 +0100

    bug fix

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
+            <BrowserRouter basename='/weather'>
                 <ThemeProvider>
                     <App />
                 </ThemeProvider>
